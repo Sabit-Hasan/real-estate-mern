1404c32f036ffe743bcc5848ccb8b4c2939fb1f5
Sabit-Hasan <hayshin.sabit@gmail.com>
2023-11-17 12:30:01 +0600

    module not found solve(.js) as import

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+
+
+const app = express();
 dotenv.config();
 
 // Connect Database with mongoose
@@ -9,13 +13,13 @@ mongoose.connect(process.env.MONGO_URL)
         console.log("Connected to MongoDB.");
     })
     .catch((err) => {
-        console.log(err)
+        console.log(err);
     });
-
-
-const app = express();
 
 // App listen
 app.listen(8000, () => {
     console.log("Server listening on port 8000");
 });
+
+// Routers Call
+app.use('/api/user', userRouter);
